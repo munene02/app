89003658a1f0d6a933193ad22d1570ba6c604c86
munene02 <munene02@gmail.com>
2017-02-08 01:19:36 +0300

    title change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -46,6 +46,7 @@
             }
 
             .title {
+                padding-top: 300px;
                 font-size: 84px;
             }
 
@@ -79,16 +80,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Soko Ile Kuu
                 </div>
-
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <p>Coming soon!</p>
                 </div>
+                
             </div>
         </div>
     </body>
