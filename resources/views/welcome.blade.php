18356c8fd8cc9039abb3259bd465784ac4fd371f
munene02 <munene02@gmail.com>
2017-02-08 03:14:04 +0300

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,15 +79,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Soko Ile Kuu
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <p>Coming soon!</p>
                 </div>
             </div>
         </div>
